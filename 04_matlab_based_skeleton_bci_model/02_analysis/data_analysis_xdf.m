d1f9a978b23effc52d6b94b07a51f785e224c4b1
Cram <c.ramirezbedoya@student.tugraz.at>
2022-10-24 00:38:11 +0200

    Added ERDS map generation

--- 04_matlab_based_skeleton_bci_model/02_analysis/data_analysis_xdf.m
+++ 04_matlab_based_skeleton_bci_model/02_analysis/data_analysis_xdf.m
@@ -10,11 +10,12 @@ filename = "trial_data.xdf";
 data = load_xdf(trial_data_location + filename, 'Verbose', true);
 
 %Check if this information fits with real streams
+%fields: info, segments, time_series, time_stamps
 usbAmp = data{1};
 channels = ["FC4", "FC1", "FCz", "FC2", "FC3", "C5", "C3", "C1", ...
     "Cz", "C2", "C4", "C6", "CP3", "CPz", "CP4", "Pz"];
 c_labels = containers.Map(channels, 1:length(channels));
-%fields: info, segments, time_series, time_stamps
+
 markers = data{2};
 %fields: info, time_series, stamps
 %%
@@ -31,21 +32,44 @@ usbAmp.time_series = filtfilt(h_bp.sosMatrix,h_bp.ScaleValues , ...
     double(usbAmp.time_series)')';
 
 %Different approach, choose theoretical epoch size
-%BCIpar = set_bciparadigm_parameters_twoclass_mi;
+BCIpar = set_bciparadigm_parameters_twoclass_mi;
 %wlength = (BCIpar.times.time_mi + BCIpar.times.time_cue  ...
 %    + BCIpar.times.time_pre_cue)*fs;
 
 %epoched_data = extract_epochs(eeg_data, eeg_times, markers);
+t_lim = [-2 5];
 [t_epoch, eegdata_epoched, t_markers] =epoch_data_xdf_streams(...
-    usbAmp, markers,"pre_cue_start", [-2, 5]); 
+    usbAmp, markers,"pre_cue_start", t_lim); 
 
 [reject_epoch, reject_chann] = perform_outlier_rejection(eegdata_epoched);
 
 if ~isempty(reject_epoch)
     valid_epochs = ~ismember(1:size(eegdata_epoched,3),reject_epoch);
     eegdata_epoched = eegdata_epoched(:,:,valid_epochs);
+    t_markers = t_markers(valid_epochs);
+    valid_labels = BCIpar.cues.class_list(valid_epochs);
 end
 
 %[C3; Cz; C4] from data
-eeg_lapl = filter_laplacian(eegdata_epoched, c_labels);
+eeg_lapl = filter_laplacian(usbAmp.time_series, c_labels);
+eeg_lapl_epoched = filter_laplacian(eegdata_epoched, c_labels);
+
+
+%ERDS-MAPS
+[~, starts] = ismembertol(t_markers, usbAmp.time_stamps, 1e-6);
+erds_header.SampleRate = fs;
+erds_header.TRIG = starts';
+erds_header.Classlabel = valid_labels;
+erds_borders = [2 40];
+
+cond = 1;
+erds_calc = calcErdsMap(eeg_lapl', erds_header, ...
+                t_lim, erds_borders, 'heading',...
+                sprintf("ERDS Maps %s", BCIpar.cues.class_labels{cond}),...
+                'cue', 0, 'class', cond, 'sig', 'boot');
+
+plotErdsMap(erds_calc);
+
+%PSD Estimation
+
 
