f751860fbdd405b56166472e5da5a5339cc6a7a2
Cram <c.ramirezbedoya@student.tugraz.at>
2022-10-27 16:34:22 +0200

    Execute feature extraction for alpha and beta band

--- 04_matlab_based_skeleton_bci_model/02_analysis/filter_csp.m
+++ 04_matlab_based_skeleton_bci_model/02_analysis/filter_csp.m
@@ -1,4 +1,4 @@
-function C = filter_csp(channel_signal,labels, window, cue_start, fs)
+function C = filter_csp(signal_windowed,labels)
 %CALC_CSP  Calculate CSP filters from signal and labels
 %   Calculates the csp filters corresponding a eeg channel signal
 %   matrix (channels x time x epochs) and its corresponding labels
@@ -6,18 +6,18 @@ function C = filter_csp(channel_signal,labels, window, cue_start, fs)
 %   each column is a spatial filter
 TRIAL_DIM = 3;
 %Use data of 1.5 - 2.5 seconds after cue presentation
-win_start = round(window(1)*fs);
-win_end = round(window(2)*fs);
-window = (cue_start + win_start):(cue_start+win_end);
-signal_windowed = channel_signal(:,window,:);
+% win_start = round(window(1)*fs);
+% win_end = round(window(2)*fs);
+% window = (cue_start + win_start):(cue_start+win_end);
+% signal_windowed = channel_signal(:,:,:);
 
 %Calculate the spatial covariance fo each epoch
 cov_epochs = zeros(size(signal_windowed,1), size(signal_windowed,1),...
-    epochs);
+    size(signal_windowed,TRIAL_DIM));
 for k_epoch=1:size(signal_windowed,TRIAL_DIM)
     nominator = signal_windowed(:,:,k_epoch)* ...
                 signal_windowed(:,:,k_epoch)';
-    cov_epochs(:,:,epoch_idx) = nominator/trace(nominator);
+    cov_epochs(:,:,k_epoch) = nominator/trace(nominator);
 end
 avg_cov_class_1 = mean(cov_epochs(:,:,labels==1),TRIAL_DIM);
 avg_cov_class_2 = mean(cov_epochs(:,:,labels==2),TRIAL_DIM);
